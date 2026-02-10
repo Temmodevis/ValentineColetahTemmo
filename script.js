35a91475017397b880f65bd3a6195b8014ad998b
Temmodevis <116628828+Temmodevis@users.noreply.github.com>
2026-02-10 22:57:55 +0300

    Refactor flower function and update noBtn behavior

--- script.js
+++ script.js
@@ -10,11 +10,34 @@ current=i;
 document.getElementById('yesBtn').onclick=()=>show(1);
 
 document.getElementById('noBtn').onmouseover=()=>{
-document.getElementById('noBtn').style.transform=
-`translate(${Math.random()*120-60}px,${Math.random()*120-60}px)`
+noBtn.style.transform=`translate(${Math.random()*80-40}px,${Math.random()*80-40}px)`
 };
 
 document.addEventListener('click',e=>{
 if(e.target.classList.contains('nextBtn')) show(current+1);
 if(e.target.classList.contains('backBtn')) show(current-1);
 });
+
+/* FLOWERS */
+
+function flower(){
+const f=document.createElement("div");
+f.innerText=["🌸","🌹","💮","🌷"][Math.floor(Math.random()*4)];
+f.style.position="fixed";
+f.style.left=Math.random()*100+"vw";
+f.style.top="-20px";
+f.style.fontSize="20px";
+f.style.opacity=".5";
+document.body.appendChild(f);
+
+let fall=setInterval(()=>{
+f.style.top=(f.offsetTop+2)+"px";
+},20);
+
+setTimeout(()=>{
+clearInterval(fall);
+f.remove();
+},8000);
+}
+
+setInterval(flower,400);
